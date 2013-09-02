2ecf19c3ed81675ddb1075caf664230aa4b8b357
吴开胜 <709392650@qq.com>
2013-09-02 17:54:59 +0800

    Delete request_uri.php

--- request_uri.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$uri = $_SERVER['REQUEST_URI'];
-	$foo = $_GET['foo'];
-	header('Content-type:text/html');
-	echo "The URI requested is :".$uri;
-	echo 'The value of the "foo" parameter is:'.$foo;
-?>
