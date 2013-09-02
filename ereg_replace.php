32240179e37555426e2864f7be3b3f698ef4f67a
吴开胜 <709392650@qq.com>
2013-09-02 17:53:59 +0800

    Delete ereg_replace.php

--- ereg_replace.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string = "This is a test";
-echo str_replace(" is", " was", $string);
-echo ereg_replace("( )is", "\\1was", $string);
-echo ereg_replace("(( )is)", "\\2was", $string);
-	
-?>
