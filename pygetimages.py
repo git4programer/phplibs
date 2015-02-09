652bb8390d97d9558845596b71f43705af553f0a
吴开胜 <kasonpasser@users.noreply.github.com>
2015-02-09 14:40:55 +0800

    Update pygetimages.py

--- pygetimages.py
+++ pygetimages.py
@@ -106,3 +106,6 @@ def writeImage(dirName,url):
  
 if __name__ == '__main__':
     getMeinv();
+
+
+#curl "http://sexy.faceks.com/sitemap.xml" |grep -o "http://.*post/.\{14\}" |xargs curl |grep "bigimgsrc" |grep -o "http://imglf.*.jpg" |xargs wget -t10 -T15 -P faceks
